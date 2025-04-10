c092ed04802636c3b08fa5fbb9b9905d6a2e52c1
LucasTL <lucasliuhaolin@gmail.com>
2025-04-10 21:58:52 +0200

    feat: init commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://lucastheliu.github.io",
+  base: "template",
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [react(), mdx(), icon()],
+});
